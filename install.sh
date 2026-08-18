f9663f3d1609975fc421f28c3a2ec619153d5085
dhalytskyi <daniilogalickij@gmail.com>
2026-08-18 10:49:34 +0200

    Add cowsay installation to install script for apt and pacman

--- install.sh
+++ install.sh
@@ -48,8 +48,10 @@ fi
 
 if command -v pacman >/dev/null 2>&1; then
     PKG_MANAGER="pacman"
+    sudo pacman -S cowsay
 elif command -v apt >/dev/null 2>&1; then
     PKG_MANAGER="apt"
+    sudo apt install cowsay
 else
     echo "Unsupported package manager (expected apt or pacman)"; exit 1
 fi
