fb4d7ad250b69412b0c29b351287c865ed1dc497
dhalytskyi <daniilogalickij@gmail.com>
2026-08-18 11:18:27 +0200

    Make cowsay installation non-interactive in install script

--- install.sh
+++ install.sh
@@ -48,10 +48,10 @@ fi
 
 if command -v pacman >/dev/null 2>&1; then
     PKG_MANAGER="pacman"
-    sudo pacman -S cowsay
+    sudo pacman -S cowsay --noconfirm
 elif command -v apt >/dev/null 2>&1; then
     PKG_MANAGER="apt"
-    sudo apt install cowsay
+    sudo apt install cowsay -y
 else
     echo "Unsupported package manager (expected apt or pacman)"; exit 1
 fi
